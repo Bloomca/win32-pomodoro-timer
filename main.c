70291c2f87d1a4bd8967f3dbefe82423d943dd80
Seva Zaikov <seva.zaikov@gmail.com>
2025-09-21 11:05:47 -0700

    add extra buttons

--- main.c
+++ main.c
@@ -2,22 +2,24 @@
 #include <dwmapi.h>
 
 #define ID_BTN_START 1001
+#define ID_BTN_RESET 1002
+#define ID_BTN_ADD_MINUTE 1003
+#define ID_BTN_REMOVE_MINUTE 1004
 #define TIMER_ID 1
 
 static UINT_PTR timerID = 0;
 static int remainingSeconds = 25 * 60;
 
 LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
-
-BOOL IsSystemDarkMode();
+void updateTimer();
 
 HWND timerHandle;
-HWND buttonHandle;
-BOOL isDarkMode;
+HWND startButtonHandle;
+HWND resetButtonHandle;
+HWND addMinuteButtonHandle;
+HWND removeMinuteButtonHandle;
 
 int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLine, int nCmdShow) {
-    isDarkMode = IsSystemDarkMode();
-
     MSG msg;
     WNDCLASSW wc = {0};
 
@@ -28,7 +30,7 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR lpCmdLin
     wc.hCursor = LoadCursor(0, IDC_ARROW);
 
     RegisterClass(&wc);
-    CreateWindow(wc.lpszClassName, L"Timer app", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 400, 400, 0, 0, hInstance, 0);
+    CreateWindow(wc.lpszClassName, L"Timer app", WS_OVERLAPPEDWINDOW | WS_VISIBLE, 100, 100, 375, 350, 0, 0, hInstance, 0);
 
     while (GetMessage(&msg, NULL, 0, 0)) {
         TranslateMessage(&msg);
@@ -44,13 +46,11 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
     switch (msg) {
         case WM_CREATE:
-            DwmSetWindowAttribute(hwnd, DWMWA_USE_IMMERSIVE_DARK_MODE, &isDarkMode, sizeof(isDarkMode));
-
             int backdropType = 2; // DWMSBT_MAINWINDOW (Mica)
             DwmSetWindowAttribute(hwnd, DWMWA_SYSTEMBACKDROP_TYPE, &backdropType, sizeof(backdropType));
 
             timerHandle = CreateWindowW(L"Static", L"25:00", WS_CHILD | WS_VISIBLE | SS_LEFT,
-                20, 20, 150, 60, 
+                125, 20, 120, 60, 
                 hwnd, (HMENU) 1, NULL, NULL);
 
             HFONT hFont = CreateFont(
@@ -68,46 +68,80 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
 
             SendMessage(timerHandle, WM_SETFONT, (WPARAM)hFont, TRUE);
 
-            buttonHandle = CreateWindowW(L"Button", L"Start",
+            startButtonHandle = CreateWindowW(L"Button", L"Start",
                 WS_VISIBLE | WS_CHILD ,
                 20, 80, 150, 25, hwnd, (HMENU) ID_BTN_START, NULL, NULL);
+
+            resetButtonHandle = CreateWindowW(L"Button", L"Reset",
+                WS_VISIBLE | WS_CHILD ,
+                190, 80, 150, 25, hwnd, (HMENU) ID_BTN_RESET, NULL, NULL);
+
+            addMinuteButtonHandle = CreateWindowW(L"Button", L"+",
+                WS_VISIBLE | WS_CHILD ,
+                250, 30, 50, 25, hwnd, (HMENU) ID_BTN_ADD_MINUTE, NULL, NULL);
+
+            removeMinuteButtonHandle = CreateWindowW(L"Button", L"-",
+                WS_VISIBLE | WS_CHILD ,
+                50, 30, 50, 25, hwnd, (HMENU) ID_BTN_REMOVE_MINUTE, NULL, NULL);
 
             break;
 
         case WM_COMMAND:
             if (LOWORD(wParam) == ID_BTN_START) {
                 wchar_t buttonText[256];
-                GetWindowTextW(buttonHandle, buttonText, 256);
+                GetWindowTextW(startButtonHandle, buttonText, 256);
 
                 if (wcscmp(buttonText, L"Start") == 0) {
-                    SetWindowTextW(buttonHandle, L"Stop");
+                    if (remainingSeconds == 0) {
+                        remainingSeconds = 25 * 60;
+                        updateTimer();
+                    }
+
+                    SetWindowTextW(startButtonHandle, L"Stop");
                     timerID = SetTimer(hwnd, TIMER_ID, 1000, NULL);
                 } else {
-                    SetWindowTextW(buttonHandle, L"Start");
+                    SetWindowTextW(startButtonHandle, L"Start");
                     if (timerID != 0) {
                         KillTimer(hwnd, TIMER_ID);
                         timerID = 0;
                     }
                 }
+            } else if (LOWORD(wParam) == ID_BTN_RESET) {
+                SetWindowTextW(startButtonHandle, L"Start");
+                if (timerID != 0) {
+                    KillTimer(hwnd, TIMER_ID);
+                    timerID = 0;
+                }
+
+                remainingSeconds = 25 * 60;
+
+                updateTimer();
+            } else if (LOWORD(wParam) == ID_BTN_REMOVE_MINUTE) {
+                if (remainingSeconds >= 60) {
+                    remainingSeconds -= 60;
+                } else {
+                    remainingSeconds = 0;
+                }
+                
+                updateTimer();
+            } else if (LOWORD(wParam) == ID_BTN_ADD_MINUTE) {
+                remainingSeconds += 60;
+                updateTimer();
             }
             break;
 
         case WM_TIMER:
             if (wParam == TIMER_ID) {
-                remainingSeconds--;
+                if (remainingSeconds > 0) {
+                    remainingSeconds--;
+                }
                 
-                int minutes = remainingSeconds / 60;
-                int seconds = remainingSeconds % 60;
-                
-                wchar_t timeString[32];
-                wsprintfW(timeString, L"%02d:%02d", minutes, seconds);
-
-                SetWindowTextW(timerHandle, timeString);
+                updateTimer();
                 
                 if (remainingSeconds <= 0) {
                     KillTimer(hwnd, TIMER_ID);
                     timerID = 0;
-                    SetWindowTextW(buttonHandle, L"Start");
+                    SetWindowTextW(startButtonHandle, L"Start");
                     
                     MessageBeep(MB_ICONINFORMATION);
                     SetWindowTextW(timerHandle, L"00:00");
@@ -115,6 +149,7 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             }
             break;
 
+        // needed for transparent background for Timer text
         case WM_CTLCOLORSTATIC:
         {
             HDC hdcStatic = (HDC)wParam;
@@ -142,29 +177,12 @@ LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam) {
     return DefWindowProcW(hwnd, msg, wParam, lParam);
 }
 
-BOOL IsSystemDarkMode() {
-    HKEY hKey;
-    BOOL darkMode = FALSE;
+void updateTimer() {
+    int minutes = remainingSeconds / 60;
+    int seconds = remainingSeconds % 60;
+    
+    wchar_t timeString[32];
+    wsprintfW(timeString, L"%02d:%02d", minutes, seconds);
 
-    if (RegOpenKeyEx(HKEY_CURRENT_USER,L"Software\\Microsoft\\Windows\\CurrentVersion\\Themes\\Personalize", 0, KEY_READ, &hKey) == ERROR_SUCCESS) {
-        DWORD size = sizeof(DWORD);
-
-        DWORD lightTheme = 1;
-        RegQueryValueExW(hKey, L"AppsUseLightTheme", NULL, NULL, 
-                        (LPBYTE)&lightTheme, &size);
-        darkMode = (lightTheme == 0) ? TRUE : FALSE;
-
-        if (darkMode == TRUE) {
-            return darkMode;
-        }
-
-        DWORD value = 1;
-        RegQueryValueExW(hKey, L"SystemUsesLightTheme", NULL, NULL, 
-                        (LPBYTE)&value, &size);
-        darkMode = (value == 0) ? 1 : 0;
-
-        RegCloseKey(hKey);
-    }
-
-    return darkMode;
+    SetWindowTextW(timerHandle, timeString);
 }
